c4a04e09e29ba5639cf15a075cdef5f3121a50ba
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@gmail.com>
2022-06-23 13:43:23 -0300

    Initialize project

---
+++ tp3_entregable/main.c
@@ -0,0 +1,18 @@
+/*
+ * tp3_entregable.c
+ *
+ * Created: 23/06/2022 01:40:58 p. m.
+ * Author : Alejandro Mejía
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
